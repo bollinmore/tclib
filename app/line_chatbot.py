6420fa93954e78de2c2ae013d7471a42981e0ad5
SuperQ <bollinmore@gmail.com>
2022-03-24 11:57:44 +0800

    Let line chatbot to accept instruction.
    Signed-off-by: SuperQ <bollinmore@gmail.com>

--- app/line_chatbot.py
+++ app/line_chatbot.py
@@ -1,11 +1,20 @@
 import logging
+import os
 
 from linebot import (
     LineBotApi, WebhookHandler
 )
-from linebot.models import TextSendMessage
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import TextSendMessage, MessageEvent, TextMessage
+from flask import request, abort
+
+line_bot_api = LineBotApi(os.environ.get('LINE_TOKEN'))
+handler = WebhookHandler(os.environ.get('LINE_SECRET'))
 
 class chatbot():
+
     def __init__(self, token, secret) -> None:
         self.line_bot_api = LineBotApi(token)
         self.handler = WebhookHandler(secret)
@@ -32,4 +41,25 @@ class chatbot():
 
         return True
 
+def callback_handler(body, signature):
+    try:
+        handler.handle(body, signature)  # handle webhook body
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    text = event.message.text
+
+    if text == '/refresh':
+        from app.tasks.fetch import fetch
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text="Scanning"))
+        fetch.delay()
+    else:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text))
         
