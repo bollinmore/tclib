904546be54f16eb1ba42d37e29c25f0789af88f3
SuperQ <bollinmore@gmail.com>
2022-06-24 09:29:59 +0800

    Chatbot - Remove /card
    Signed-off-by: SuperQ <bollinmore@gmail.com>

--- app/line_chatbot.py
+++ app/line_chatbot.py
@@ -64,22 +64,6 @@ def handle_text_message(event):
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text="Scanning"))
         fetch.delay()
-    elif text == '/card':
-        url = "{}/barcode".format(request.root_url)
-        buttons_template = ButtonsTemplate(
-            title='行動借閱證', text='...', actions=[
-                URIAction(label=u'陳文昇', uri='{}/L123728869'.format(url)),
-                URIAction(label=u'林佳嬅', uri='{}/M222033097'.format(url)),
-                URIAction(label=u'陳祈安', uri='{}/F132168386'.format(url)),
-                URIAction(label=u'陳怡安', uri='{}/F231624332'.format(url)),
-                # URIAction(label=u'家庭卡', uri='{}/BC275509'.format(url)),
-                # PostbackAction(label='ping', data='ping'),
-                # PostbackAction(label='ping with text', data='ping', text='ping'),
-                # MessageAction(label='Translate Rice', text='米')
-            ])
-        template_message = TemplateSendMessage(
-            alt_text='Buttons alt text', template=buttons_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
     else:
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text=event.message.text))
